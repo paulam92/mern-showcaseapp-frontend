864e31b14f52cde203edefd79777a3572e7327ca
Nemesis <szakacsmpaula@gmail.com>
2021-12-08 14:57:09 +0100

    change scss

--- src/pages/PageLogin.js
+++ src/pages/PageLogin.js
@@ -1,5 +1,18 @@
+import { useContext } from "react";
+import AppContext from "../AppContext";
+
 const PageLogin = () => {
-  return <>This is the login page</>;
-};
+    const { siteStatus, toggleStatus } = useContext(AppContext);
+    return (
+      <div>
+        <p> This is the register page
+          The current status is: <span className="highlight">{siteStatus}</span>
+        </p>
+        <p>
+          <button onClick={toggleStatus}>Toggle Status</button>
+        </p>
+      </div>
+    );
+  };
 
 export default PageLogin;
